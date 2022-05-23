6c40708f5d53665f321c2dc5dfedefffd74d9b8c
Giuseppe Petraroli <g.petraroli@gmail.com>
2022-05-23 14:16:12 +0200

    quettes 12 and 13

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -4,8 +4,11 @@ namespace App\Controller;
 
 use App\Entity\Category;
 use App\Entity\Program;
+use App\Form\CategoryType;
+use App\Repository\CategoryRepository;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -18,6 +21,26 @@ class CategoryController extends AbstractController
         $categories = $doctrine->getRepository(Category::class)->findAll();
 
         return $this->render('category/index.html.twig', ['categories' => $categories]);
+    }
+
+    #[Route('/new', name: 'new')]
+    public function new(Request $request, CategoryRepository $categoryRepository): Response
+    {
+        $category = new Category();
+
+        $form = $this->createForm(CategoryType::class, $category);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted()) {
+            $categoryRepository->add($category, true);
+
+            return $this->redirectToRoute('category_index');
+        }
+
+        return $this->renderForm('category/new.html.twig', [
+            'form' => $form,
+        ]);
     }
 
     #[Route('/{categoryName}', name: 'show', methods: ['GET'])]
--- src/Controller/ProgramController.php
+++ src/Controller/ProgramController.php
@@ -2,10 +2,15 @@
 
 namespace App\Controller;
 
+use App\Entity\Episode;
 use App\Entity\Program;
 use App\Entity\Season;
+use App\Form\ProgramType;
+use App\Repository\ProgramRepository;
 use Doctrine\Persistence\ManagerRegistry;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Entity;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -19,14 +24,31 @@ class ProgramController extends AbstractController
         return $this->render('program/index.html.twig', ['programs' => $programs]);
     }
 
-    #[Route('/program/{id<\d+>}', name: 'program_show', methods: ['GET'])]
-    public function show(ManagerRegistry $doctrine, $id): Response
+    #[Route('/program/new', name: 'program_new', methods: ['GET', 'POST'])]
+    public function new(Request $request, ProgramRepository $programRepository): Response
     {
-        $program = $doctrine->getRepository(Program::class)->findOneBy(['id' => $id]);
+        $program = new Program();
 
+        $form = $this->createForm(ProgramType::class, $program);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted()) {
+            $programRepository->add($program, true);
+
+            return $this->redirectToRoute('program_index');
+        }
+
+        return $this->renderForm('program/new.html.twig', ['form' => $form]);
+    }
+
+    #[Route('/program/{id<\d+>}', name: 'program_show', methods: ['GET'])]
+    public function show(Program $program): Response
+    {
+        // using param converter to retrieve $program
         if (!$program) {
             throw $this->createNotFoundException(
-                'No program with id : '.$id.' found in program\'s table.'
+                'No program with id : '.$program.' found in program\'s table.'
             );
         }
 
@@ -39,21 +61,30 @@ class ProgramController extends AbstractController
     }
 
     #[Route('/program/{programId}/season/{seasonId<\d+>}', name: 'program_season_show', methods: ['GET'])]
-    public function showSeason(ManagerRegistry $doctrine ,int $programId, int $seasonId): Response
+    #[Entity('program', options: ['id' => 'programId'])]
+    #[Entity('season', options: ['id' => 'seasonId'])]
+    public function showSeason(Program $program, Season $season): Response
     {
-        $program = $doctrine->getRepository(Program::class)->findOneBy(['id' => $programId]);
-
-        $season = $doctrine->getRepository(Season::class)->findOneBy([
-            'id' => $seasonId,
-            'program' => $program->getId(),
-        ]);
-
+        // using param converter to retrieve $program and $season
         $episodes = $season->getEpisodes();
 
         return $this->render('program/season_show.html.twig', [
             'program' => $program,
             'season' => $season,
             'episodes' => $episodes,
+        ]);
+    }
+
+    #[Route('/program/{programId}/season/{seasonId}/episode/{episodeId}', name: 'program_episode_show', methods: ['GET'])]
+    #[Entity('program', options: ['id' => 'programId'])]
+    #[Entity('season', options: ['id' => 'seasonId'])]
+    #[Entity('episode', options: ['id' => 'episodeId'])]
+    public function showEpisode(Program $program, Season $season, Episode $episode): Response
+    {
+        return $this->render('program/episode_show.html.twig', [
+            'program' => $program,
+            'season' => $season,
+            'episode' => $episode,
         ]);
     }
 }
